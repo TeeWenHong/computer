abacfd3755382900167aebb324b2d4ba72d78b23
TeeWenHong <61152364+TeeWenHong@users.noreply.github.com>
2020-05-24 03:16:14 +0800

    homework2.1

---
+++ homework2.1/homework2.1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace homework2._1
+{
+    class Program
+    {
+        public static String getResult(long num)
+        {
+            StringBuilder count = new StringBuilder();
+           
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+               
+                while (true)
+                {
+                    
+                    if (num % i == 0)
+                    {
+                        count.Append(i);
+                        count.Append(" ");
+                        num /= i;
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+            }
+            if (num != 1)
+            {
+                count.Append(num);
+                count.Append(" ");
+            }
+            return count.ToString();
+        }
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("input the number");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("answer");
+            Console.WriteLine(getResult(num));
+        }
+    }
+}
