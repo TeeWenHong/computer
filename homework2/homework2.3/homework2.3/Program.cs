20319bb5eaef1e0b6901ad70079c83506b76a4a6
TeeWenHong <61152364+TeeWenHong@users.noreply.github.com>
2020-05-24 13:39:25 +0800

    homework2.3

---
+++ homework2/homework2.3/homework2.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[101];
+            for (int i=2; i<=100; i++) {
+                if (a[i] == 1){
+                    continue;
+                }
+                for (int x=i+1;x<=100;x++) {
+                    if (x % i == 0) {
+                        a[x] = 1;
+                    }
+                }
+                Console.WriteLine(i);
+            }          
+        }
+    }
+}
