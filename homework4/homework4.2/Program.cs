4135c13fa0aa07b3e8ccecc1abc1955df4a1669f
TeeWenHong <61152364+TeeWenHong@users.noreply.github.com>
2020-05-27 20:16:12 +0800

    homework4

---
+++ homework4/homework4.2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace homework4._2
+{
+    public delegate void Alarm(int h, int m, int s);
+    public delegate void Tick(int h, int m, int s);
+    public class Clock
+    {
+        public event Alarm Alarm;
+        public event Tick Tick;
+        private int hour;
+        private int minute;
+        private int second;
+        private int alarm_hour;
+        private int alarm_minute;
+        private int alarm_second;
+        public void Start()
+        {
+            this.hour   = DateTime.Now.Hour;
+            this.minute = DateTime.Now.Minute;
+            this.second = DateTime.Now.Second;
+        }
+        public void ClockTick()
+        {
+            this.hour   = DateTime.Now.Hour;
+            this.minute = DateTime.Now.Minute;
+            this.second = DateTime.Now.Second;
+            if (this.hour == this.alarm_hour && this.minute == this.alarm_minute && this.second == this.alarm_second)
+            {
+                return;
+            }
+            else
+            {
+                Tick(this.hour, this.minute, this.second);
+            }
+        }
+        public void ClockAlarm()
+        {
+            if (this.hour == this.alarm_hour && this.minute == this.alarm_minute && this.second == this.alarm_second) {
+                Alarm(this.hour, this.minute, this.second);
+            }
+        }
+        public void SetAlarm(int h, int m, int s)
+        {
+            this.alarm_hour = h;
+            this.alarm_minute = m;
+            this.alarm_second = s;
+        }
+    }
+    public class Form
+    {
+        public Clock clock = new Clock();
+        public Form()
+        {
+            clock.Alarm += new Alarm(Clock_Alarm);
+            clock.Tick += new Tick(Clock_Tick);
+        }
+        void Clock_Alarm(int h, int m, int s)
+        {
+            Console.WriteLine("Time up！！！");
+        }
+        void Clock_Tick(int h, int m, int s)
+        {
+            Console.WriteLine(h + ":H " + m + ":M " + s + ":S");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form f = new Form();
+            f.clock.Start();
+            f.clock.SetAlarm(16, 0, 0);
+            for (int i = 0; ; i++)
+            {
+                f.clock.ClockTick();
+                Thread.Sleep(1000);
+                f.clock.ClockAlarm();
+            }
+        }
+    }
+}
---
+++ homework4/homework4/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework4._1
+{ 
+    public class N<T>
+    {
+        public N<T> Next { 
+            get; 
+            set; 
+        }
+        public T Data { 
+            get; 
+            set; 
+        }
+        public N(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class List<T>
+    {
+        private N<T> head;
+        private N<T> tail;
+        public List() //the list is null
+        {
+            tail = head = null;
+        }
+        public N<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            N<T> n = new N<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> act)
+        {
+            if (act == null)
+            {
+                throw new ArgumentNullException(nameof(act));
+            }
+
+            N<T> head = Head;
+            while (head != null)
+            {
+                act(head.Data);
+                head = head.Next;
+                if (head == null)
+                {
+                    break;
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = -100;
+            int min = 100;
+            int sum = 0;
+            List<int> list = new List<int>();
+            Console.WriteLine("[1,3,5,2,4,6]");
+            list.Add(1);
+            list.Add(3);
+            list.Add(5);
+            list.Add(2);
+            list.Add(4);
+            list.Add(6);
+
+            list.ForEach((x) =>{if (x > max) { max = x;}if (x < min) { min = x; }sum += x;});
+            Console.WriteLine("Max:" + max);
+            Console.WriteLine("Min:" + min);
+            Console.WriteLine("Sum:" + sum);
+        }
+    }
+}
